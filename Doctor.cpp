c00cee7bf74a9d1f7cdb6bfb600c29d60f44c635
Basmala Nabil <154525200+Basmala-Nabil@users.noreply.github.com>
2024-12-22 16:57:17 +0200

    Update Doctor.cpp

--- Doctor.cpp
+++ Doctor.cpp
@@ -1,22 +1,21 @@
 #include "Doctor.h"
 
 
+// Default constructor for the Doctor class
+Doctor::Doctor():ID(""),Name(""),Duration(0.0),Rate(0.0){}// Initializes ID and Name as empty strings, and Duration and Rate as zero
 
-Doctor::Doctor():ID(""),Name(""),Duration(0.0),Rate(0.0){}
-
-
+// Parameterized constructor for the Doctor class
 Doctor::Doctor(string id , string name ,double duration ,double rate ):ID(id),Name(name),Duration(duration),Rate(rate){}
 
-
+// Function to calculate the total fee
 double Doctor::calculateTotalFee()
 {
-    return 0.0;
+    return 0.0;// Currently returns 0.0; should be overridden in derived classes
 }
 
-
+// Function to return the doctor's information as a formatted string
 string Doctor::PrettyPrint()
 {
     return string("Doctor ID is ")+ID+string(" and name is Dr. ")+Name;
 }
-
 
