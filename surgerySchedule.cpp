7a56f9ff2c2043a1a608770398d49a0e7250df59
Basmala Nabil <154525200+Basmala-Nabil@users.noreply.github.com>
2024-12-22 16:57:56 +0200

    Update surgerySchedule.cpp

--- surgerySchedule.cpp
+++ surgerySchedule.cpp
@@ -1,26 +1,28 @@
 #include "surgerySchedule.h"
 
+// Default constructor
 surgerySchedule::surgerySchedule():patientName(""),Date(""){}
 
+// Parameterized constructor
 surgerySchedule::surgerySchedule(string patient_name , string date):patientName(patient_name),Date(date){}
 
-
+// Setter for patientName
 void surgerySchedule::setpatientName(string name)
 {
     patientName = name;
 }
-
+// Setter for Date
 void surgerySchedule::setdate(string date)
 {
     Date = date;
 }
-
+// Getter for patientName
 string surgerySchedule::getpatientName()const
 {
     return patientName;
 }
+// Getter for Date
 string surgerySchedule::getdate()const
 {
     return Date;
 }
-
