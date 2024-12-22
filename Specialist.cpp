f67f411cfbc2a0d748c201a00da71d2b2303ae2b
Basmala Nabil <154525200+Basmala-Nabil@users.noreply.github.com>
2024-12-22 16:59:28 +0200

    Update Specialist.cpp

--- Specialist.cpp
+++ Specialist.cpp
@@ -1,19 +1,20 @@
 #include "Specialist.h"
 
+// Default constructor
 Specialist::Specialist() : Doctor(0,"",0.0,0.0),Speciality(""){}
 
-
+// Parameterized constructor
 Specialist::Specialist(string speciality, string id , string name , double duration , double rate ):Doctor(id,name,duration,rate),Speciality(speciality){}
 
 
-
+// Function to calculate the total fee
 double Specialist::calculateTotalFee()
 {
     return (Duration / 15) * Rate;
 }
 
 
-
+// Function to return a formatted string with the specialist's details
 string Specialist::PrettyPrint()
 {
     return string("Dr. ")+Name+string(" is a Specialist with whose total  fee is ")+to_string(calculateTotalFee());
